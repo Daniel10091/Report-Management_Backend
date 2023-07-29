b35c75b815e6c81cacb3555cd8e09a58073f9bb3
Daniel Aguiar <dc03934@gmail.com>
2023-07-29 19:30:02 -0300

    Created Person Entity

---
+++ report/src/main/java/com/example/report/domain/model/Person.java
@@ -0,0 +1,46 @@
+package com.example.report.domain.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(nullable = false)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String phone;
+
+  @Column(name = "created_date", nullable = false)
+  @CreationTimestamp()
+  private LocalDateTime createdDate;
+
+  @Column(name = "updated_date", nullable = false)
+  @CreationTimestamp()
+  private LocalDateTime updatedDate;
+  
+}
