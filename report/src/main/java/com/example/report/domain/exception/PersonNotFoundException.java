247cf77186b5a3808495d5c131ab427af40613a8
Daniel Aguiar <dc03934@gmail.com>
2023-07-29 22:03:55 -0300

    Created Person Exceptions

---
+++ report/src/main/java/com/example/report/domain/exception/PersonNotFoundException.java
@@ -0,0 +1,20 @@
+package com.example.report.domain.exception;
+
+public class PersonNotFoundException extends RuntimeException {
+  
+  /**
+   * @param message
+   */
+  public PersonNotFoundException(String message) {
+    super(message);
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public PersonNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
