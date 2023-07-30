3c8d82386ae3a68f3128fc568b44e62f9073958a
Daniel Aguiar <dc03934@gmail.com>
2023-07-29 21:07:19 -0300

    Created Person Mapper

---
+++ report/src/main/java/com/example/report/domain/mapper/PersonMapper.java
@@ -0,0 +1,26 @@
+package com.example.report.domain.mapper;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.example.report.domain.dto.PersonDTO;
+import com.example.report.domain.model.Person;
+
+@Mapper
+public class PersonMapper {
+  
+  @Mapping(source = "person.id", target = "code")
+  @Mapping(source = "person.user.avatarPath", target = "avatarPath")
+  @Mapping(source = "person.user.themeImagePath", target = "themeImagePath")
+  @Mapping(source = "person.user.active", target = "active")
+  static PersonDTO toDto(Person Person) {
+    throw new UnsupportedOperationException("Unimplemented method 'toDto'");
+  }
+
+  @InheritInverseConfiguration
+  static Person toEntity(PersonDTO PersonDTO) {
+    throw new UnsupportedOperationException("Unimplemented method 'toEntity'");
+  }
+
+}
