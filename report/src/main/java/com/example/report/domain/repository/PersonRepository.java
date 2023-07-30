51723a809350bb6320b753c6707b9ce9585e5ccc
Daniel Aguiar <dc03934@gmail.com>
2023-07-29 22:04:18 -0300

    Created Person Repository

--- report/src/main/java/com/example/report/domain/repository/PersonRepository.java
+++ report/src/main/java/com/example/report/domain/repository/PersonRepository.java
@@ -1,6 +1,10 @@
 package com.example.report.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.report.domain.model.Person;
@@ -8,4 +12,16 @@ import com.example.report.domain.model.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
   
+  Optional<Person> findById(Long id);
+
+  @Query(" SELECT person FROM Person person " +
+         " WHERE person.firstName LIKE '%:firstName%' " +
+         " AND person.lastName LIKE '%:lastName%'")
+  Optional<Person> findPersonByName(
+    @Param("firstName") String firstName,
+    @Param("lastName") String lastName
+  );
+
+  Optional<Person> findPersonByFirstNameAndLastName(String firstName, String lastName);
+
 }
